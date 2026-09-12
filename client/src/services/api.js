61a4e47833a90a00b46d605c696de5a87e6241b5
Guru Bhavishya <gurubhavishya33@gmail.com>
2026-09-12 13:06:24 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://movie-watchlist-backend-46h8.onrender.com/api",
 });
 
 export default api;
